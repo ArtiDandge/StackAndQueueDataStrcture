0585e8d2ead7d7c75fe148a1d1c7263d62aa6e9d
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-16 17:01:02 +0530

    [Arti Dandge] Add : Ability to create Queue and add elements to it and display them

---
+++ StackAndQueueDataStrcture/LinkedListQueue.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StackAndQueueDataStrcture
+{
+    public class LinkedListQueue
+    {
+        internal Node head;
+        /// <summary>
+        /// Method to add elements to the Queue
+        /// </summary>
+        /// <param name="data"></param>
+        internal void Enqueue(int data)
+        {
+            Node node = new Node(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node temp = head;
+                while (temp.next != null)
+                {
+                    temp = temp.next;
+                }
+                temp.next = node;
+                Console.WriteLine("\n{0} inserted into Queue", node.data);
+            }
+
+        }
+
+        /// <summary>
+        /// Method to display Queue 
+        /// </summary>
+        internal void Display()
+        {
+            Node temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("\nQueue is Empty");
+                return;
+            }
+            Console.WriteLine("\nNow Queue has following data");
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+            Console.WriteLine("\n");
+        }
+    }
+}
--- StackAndQueueDataStrcture/Program.cs
+++ StackAndQueueDataStrcture/Program.cs
@@ -8,6 +8,7 @@ namespace StackAndQueueDataStrcture
         {
             Console.WriteLine("--------Welcome to the Project on Stacks and Queue Data Structure---------");
             LinkedListStack stack = new LinkedListStack();
+            Console.WriteLine("-------------------Stack Operations------------------");
             stack.Push(70);
             stack.Push(30);
             stack.Push(56);
@@ -17,6 +18,15 @@ namespace StackAndQueueDataStrcture
             stack.Peek();
             stack.IsEmpty();
             stack.Display();
+            Console.WriteLine("-------------------Stack Operations End------------------");
+            Console.WriteLine("\n");
+            Console.WriteLine("-------------------Queue Operations------------------");
+            LinkedListQueue queue = new LinkedListQueue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
+            Console.WriteLine("-------------------Queue Operations End------------------");
         }
     }
 }
