95e085695c36de61a0438588b2bd275dcd4ddbf2
ArtiDandge <58045752+ArtiDandge@users.noreply.github.com>
2020-12-16 16:28:19 +0530

    [Arti Dandge] Add : Welcome message on console

---
+++ StackAndQueueDataStrcture/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace StackAndQueueDataStrcture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------Welcome to the Project on Stacks and Queue Data Structure---------");
+            
+        }
+    }
+}
